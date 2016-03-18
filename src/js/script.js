1f7564f4b9af5521624cab3261bfe824f7a3d19b
Vladislav <v.kirbaba@gmail.com>
2016-03-18 14:38:49 +0300

    Slider

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 30,
+        loop: true
+    });
+});
