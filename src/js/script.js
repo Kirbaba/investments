b4073f337f146f82de936fb8667a6ad5af92476e
Vladislav <v.kirbaba@gmail.com>
2016-03-18 17:07:41 +0300

    Calculator

--- src/js/script.js
+++ src/js/script.js
@@ -8,4 +8,49 @@ $(document).ready(function(){
         spaceBetween: 30,
         loop: true
     });
+
+   jQuery(function ($) {
+	    jQuery('.savemoney-slider').slick({
+	        dots: false,
+	        infinite: true,
+	        autoplay: true,
+	        adaptiveHeight: true,
+	        speed: 300,
+	        arrows: true,        
+	        slidesToShow: 1,
+	        focusOnSelect: false,
+	        slidesToScroll: 1
+		});		
+
+	});
+
+   $('.calculator--range--month--procents').css(
+   {
+   	"width": $('.calculator--range--month .irs-grid').width()
+   });
+
+});
+
+jQuery(function ($) {
+   jQuery("#range_50a").ionRangeSlider({
+	    type: "single",
+	    values: ["100т.", "500т.", "1 млн.", "5 млн."],
+	    decorate_both: false,
+	    grid: true,
+	    force_edges: true,
+	    grid_snap: true,
+	    hide_min_max: true,
+	    hide_from_to: true
+	});
+   jQuery("#range_50").ionRangeSlider({
+	    type: "single",
+	    values: ["6", "12", "24"],
+	    decorate_both: false,
+	    grid: true,
+	    force_edges: true,
+	    grid_snap: true,
+	    hide_min_max: true,
+	    hide_from_to: true
+	});
+
 });
